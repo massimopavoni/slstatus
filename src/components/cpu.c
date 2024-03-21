9bea16d16ab464d129a739a06e511499071481e3
Massimo Pavoni <maspavoni@gmail.com>
2024-03-21 22:04:28 +0100

    spaces and tabs and spaces and tabs

--- src/components/cpu.c
+++ src/components/cpu.c
@@ -52,8 +52,8 @@
 				return -1;
 
 			shared_perc = (int)(100 *
-							((b[0] + b[1] + b[2] + b[5] + b[6]) -
-							(a[0] + a[1] + a[2] + a[5] + a[6])) / sum);
+					((b[0] + b[1] + b[2] + b[5] + b[6]) -
+					 (a[0] + a[1] + a[2] + a[5] + a[6])) / sum);
 		}
 
 		*perc = shared_perc;
